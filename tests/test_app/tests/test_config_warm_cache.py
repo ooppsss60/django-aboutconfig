a38214bb5557cc7f6cca2144cd2e07743a336a36
Kirill Stepanov <mail@kirillstepanov.me>
2016-08-23 19:57:40 -0400

    Added support for warming cache from the command line

---
+++ tests/test_app/tests/test_config_warm_cache.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+try:
+    from unittest.mock import patch
+except ImportError:
+    from mock import patch
+
+from django.core.management import call_command
+
+
+class ConfigCacheWarmTest(TestCase):
+    @patch('aboutconfig.utils.preload_cache')
+    def test_run(self, preload_cache):
+        call_command('config_warm_cache')
+        preload_cache.assert_called_once()
