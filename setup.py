a70a14818200a6cb738b4a25f075776b95c16a06
Kirill Stepanov <mail@kirillstepanov.me>
2017-03-05 22:47:19 +0900

    Update for python 3.6

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     install_requires=[
         'Django>=1.8',
