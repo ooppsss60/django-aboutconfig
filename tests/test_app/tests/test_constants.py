f332ad8f89ee0bb39eaaf4dbb0aea005cb3cf96a
Kirill Stepanov <mail@kirillstepanov.me>
2020-03-23 23:22:53 +0800

    support django 3

--- tests/test_app/tests/test_constants.py
+++ tests/test_app/tests/test_constants.py
@@ -1,20 +1,15 @@
 from unittest import TestCase
 
-from django.utils import six
-
 from aboutconfig.constants import KEY_REGEX
 
 
 class KeyRegexTest(TestCase):
     def test_run(self):
-        assertRegex = self.assertRegex if six.PY3 else self.assertRegexpMatches
-        assertNotRegex = self.assertNotRegex if six.PY3 else self.assertNotRegexpMatches
-
-        assertRegex("a", KEY_REGEX)
-        assertRegex("a.b", KEY_REGEX)
-        assertRegex("aa.bbb.CCC.1.2.3._", KEY_REGEX)
-        assertNotRegex("", KEY_REGEX)
-        assertNotRegex(".", KEY_REGEX)
-        assertNotRegex("a.", KEY_REGEX)
-        assertNotRegex(" ", KEY_REGEX)
-        assertNotRegex(" a.b", KEY_REGEX)
+        self.assertRegex("a", KEY_REGEX)
+        self.assertRegex("a.b", KEY_REGEX)
+        self.assertRegex("aa.bbb.CCC.1.2.3._", KEY_REGEX)
+        self.assertNotRegex("", KEY_REGEX)
+        self.assertNotRegex(".", KEY_REGEX)
+        self.assertNotRegex("a.", KEY_REGEX)
+        self.assertNotRegex(" ", KEY_REGEX)
+        self.assertNotRegex(" a.b", KEY_REGEX)
